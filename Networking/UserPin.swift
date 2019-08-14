105f0f3dad4d8f7f9317ce95c92201afe79aef56
ProstoOleg <o.grim2015@yandex.ru>
2019-08-14 14:16:24 +0300

    region interaction

--- Networking/UserPin.swift
+++ Networking/UserPin.swift
@@ -8,14 +8,60 @@
 
 import Foundation
 import MapKit
-class UserPin: NSObject, MKAnnotation{
+
+
+
+class UserPin: NSObject, MKAnnotation, Codable{
+   
     var coordinate: CLLocationCoordinate2D
-    var subtitle: String?
     var title: String?
-    init(coordinate:CLLocationCoordinate2D ,subtitle:String ,title: String) {
-        self.coordinate = coordinate
-        self.subtitle = subtitle
-        self.title = title
+    var radius: CLLocationDistance
+    
+    enum CodingKeys: String, CodingKey{
+       case latitude, longitude, title, radius
     }
     
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(coordinate.latitude, forKey: .latitude)
+        try container.encode(coordinate.longitude, forKey: .longitude)
+        try container.encode(radius, forKey: .radius)
+        try container.encode(title, forKey: .title)
+    
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let latitude = try values.decode(Double.self, forKey: .latitude)
+        let longitude = try values.decode(Double.self, forKey: .longitude)
+        coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        radius = try values.decode(Double.self, forKey: .radius)
+        title = try values.decode(String.self, forKey: .title)
+    }
+    
+    
+    
+
+    
+    init(coordinate:CLLocationCoordinate2D  ,title: String, radius:CLLocationDistance) {
+        self.coordinate = coordinate
+ 
+        self.title = title
+        self.radius = radius
+    }
+    
+    
+
+    
+}
+
+extension UserPin {
+    public class func allPins() -> [UserPin] {
+        guard let savedData = UserDefaults.standard.data(forKey: PreferencesKeys.savedItems) else { return [] }
+        let decoder = JSONDecoder()
+        if let savedPins = try? decoder.decode(Array.self, from: savedData) as [UserPin] {
+            return savedPins
+        }
+        return []
+    }
 }
--- Networking/source/MapViewController.swift
+++ Networking/source/MapViewController.swift
@@ -10,79 +10,143 @@ import UIKit
 import MapKit
 import CoreLocation
 
+struct PreferencesKeys {
+    static let savedItems = "savedItems"
+}
+
 class MapViewController: UIViewController {
+//MARK - Properties
+    
+    var pins = [UserPin]()
+    
     @IBOutlet weak var locationName: UITextField!
     
     let locationManager = CLLocationManager()
+    
     @IBOutlet weak var mapView: MKMapView!{
-        
         didSet{
             mapView.mapType = .standard
-            
         }
     }
     
+//MARK - Funcs
+
+    @IBAction func removePins(_ sender: Any) {
+        for pin in pins{
+            mapView.removeAnnotation(pin)
+        }
+        pins.removeAll()
+        stopMonitoring()
+        saveAllPins()
+    }
+//MARK - REGIONS
+    func regions(with pin: UserPin) -> CLCircularRegion{
+        let region = CLCircularRegion(center: pin.coordinate, radius: pin.radius, identifier: pin.title!)
+        return region
+    }
     
-    func clearReg(){
+    
+    func monitoreRegion(pin: UserPin){
+        let fenceRegion = regions(with: pin)
+        locationManager.startMonitoring(for: fenceRegion)
+    }
+    func stopMonitoring(){
         let regions = locationManager.monitoredRegions
         for region in regions{
+           
             locationManager.stopMonitoring(for: region)
         }
         
     }
     
+    func add(_ pin:UserPin){
+    pins.append(pin)
+    mapView.addAnnotation(pin)
+    locationName.text! = ""
+    monitoreRegion(pin: pin)
+    saveAllPins()
+    }
+    
+    
+
+    
+    func loadAllPins() {
+        pins.removeAll()
+        let allpins = UserPin.allPins()
+        allpins.forEach { add($0) }
+    }
+    
+    func saveAllPins(){
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(pins)
+            UserDefaults.standard.set(data, forKey: PreferencesKeys.savedItems)
+        } catch {
+            print("error encoding geotifications")
+        }
+    }
+    
+    
+   
         override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
-        clearReg()
-            
+     
         locationManager.requestAlwaysAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
-        
+        loadAllPins()
+        }
     
-        
-        // Do any additional setup after loading the view.
-    }
+//MARK - Adding pin to map
     
     @IBAction func addPin(_ sender: UITapGestureRecognizer) {
-        self.mapView.removeAnnotations(mapView.annotations)
+        if locationName.text != ""{
         let location = sender.location(in: self.mapView)
         let coordinate = self.mapView.convert(location, toCoordinateFrom: self.mapView)
-        let anotation = MKPointAnnotation()
-        anotation.coordinate = coordinate
-        anotation.title = locationName.text!
-        anotation.subtitle = ""
-        self.mapView.addAnnotation(anotation)
-         let geoFence = CLCircularRegion(center: anotation.coordinate, radius: 10000, identifier: anotation.title!)
+        let radius = 10000.0
+        let title = locationName.text!
+        let pin = UserPin(coordinate: coordinate,  title: title, radius: radius)
+        add(pin)
+            
+        }
+        else{
+            print("nope")
+        }
         
+        /*
+        
+         let geoFence = CLCircularRegion(center: anotation.coordinate, radius: 1000, identifier: anotation.title!)
         locationManager.startMonitoring(for: geoFence)
         locationName.text! = ""
+        */
     }
 
 }
 
+//MARK - Delegation
 extension MapViewController : CLLocationManagerDelegate{
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first{
             print(location.coordinate)
         }
     }
+    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if (status == CLAuthorizationStatus.denied){
             print("nope")
-            
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-      
-        print(region.identifier)
-        let alert = UIAlertController(title: "Entered Location", message: "you entered \(region.identifier)", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-        self.present(alert, animated: true)
-        
+        if region is CLCircularRegion {
+            let alert = UIAlertController(title: "You entered location", message: "You entered:\(region.title)", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        }
     }
     
-    
 }
+
+
+
+
