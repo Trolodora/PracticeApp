f45d2c8cbae7ef342f2513a1eac69a4e38f43202
ProstoOleg <o.grim2015@yandex.ru>
2019-08-23 00:02:27 +0300

    login feature in progress, todo touch id

--- Networking/Controls/LoginViewController.swift
+++ Networking/Controls/LoginViewController.swift
@@ -8,37 +8,84 @@
 
 import UIKit
 
+struct KeychainConfiguration {
+    static let serviceName = "Networking"
+    static let accessGroup: String? = nil
+}
+
 class LoginViewController: UIViewController {
+    
+    var passwordItems: [KeychainPasswordItem] = []
     @IBOutlet weak var usserPass: UITextField!
-    let pasKey = "a"    
+    let createLoginButtonTag = 0
+    let loginButtonTag = 1
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    func checkLogin(password: String) -> Bool{
-        return password == pasKey
-    }
-    @IBAction func presLog(_ sender: Any) {
-        if checkLogin(password: usserPass.text!){
-            performSegue(withIdentifier: "show", sender: self)
+        let hasLogin = UserDefaults.standard.bool(forKey: "hasLoginKey")
+    
+        if hasLogin {
+            loginButton.setTitle("OK", for: .normal)
+            loginButton.tag = loginButtonTag
+        } else {
+            loginButton.setTitle("Create", for: .normal)
+            loginButton.tag = createLoginButtonTag
         }
-        else{
-            print("ban")
+    }
+    
+    private func showLoginFailedAlert() {
+        let alertView = UIAlertController(title: "Login Problem",
+                                          message: "Wrong password.",
+                                          preferredStyle:. alert)
+        let okAction = UIAlertAction(title: "Try again", style: .default)
+        alertView.addAction(okAction)
+        present(alertView, animated: true)
+    }
+    
+    func checkLogin(password: String) -> Bool{
+        do {
+            let passwordItem = KeychainPasswordItem(service: KeychainConfiguration.serviceName,
+                                                    account: "user",
+                                                    accessGroup: KeychainConfiguration.accessGroup)
+            let keychainPassword = try passwordItem.readPassword()
+            return password == keychainPassword
+        } catch {
+            fatalError("Error reading password from keychain - \(error)")
+        }
+    }
+    
+    
+    @IBAction func presLog(_ sender: UIButton) {
+        guard let newPassword = usserPass.text,
+            !newPassword.isEmpty else {
+                showLoginFailedAlert()
+                return
+        }
+        usserPass.resignFirstResponder()
+        if sender.tag == createLoginButtonTag {
+            do {
+                let passwordItem = KeychainPasswordItem(service: KeychainConfiguration.serviceName, account: "user",
+                                                        accessGroup: KeychainConfiguration.accessGroup)
+                try passwordItem.savePassword(newPassword)
+            } catch {
+                fatalError("Error updating keychain - \(error)")
+            }
+            UserDefaults.standard.set(true, forKey: "hasLoginKey")
+            loginButton.tag = loginButtonTag
+            performSegue(withIdentifier: "show", sender: self)
+        } else if sender.tag == loginButtonTag {
+            if checkLogin(password: newPassword) {
+                performSegue(withIdentifier: "show", sender: self)
+            } else {
+                showLoginFailedAlert()
+            }
         }
         
     }
     
-
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
 }
---
+++ Networking/source/RoundButton.swift
@@ -0,0 +1,35 @@
+//
+//  RoundButton.swift
+//  Networking
+//
+//  Created by RomaDUlbich on 8/22/19.
+//  Copyright © 2019 Oleg1. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+             self.layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
